10d71013d4030b516df44aeb48fc46b14d96de4e
David Munoz Tord <david.munoztord@mailbox.org>
2026-01-22 11:30:08 +0000

    version 0.7.4

--- inst/doc/circle.R
+++ inst/doc/circle.R
@@ -1,8 +1,18 @@
 ## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
-  comment = "#>"
+  comment = "#>",
+  eval = !identical(Sys.getenv("CI"), "true") && !identical(Sys.getenv("CRAN"), "true")
 )
+
+## ----eval=FALSE---------------------------------------------------------------
+# circle::get_circle_user()
+
+## ----eval=FALSE---------------------------------------------------------------
+# circle::enable_repo()
+
+## ----eval=FALSE---------------------------------------------------------------
+# circle::use_circle_deploy()
 
 ## ----echo=FALSE---------------------------------------------------------------
 knitr::include_graphics("../man/figures/user-key.png")
